de19c8b19e070d2b7a24baca3df3d36714b45f18
Ecodrin <142536645+Ecodrin@users.noreply.github.com>
2024-11-26 17:15:30 +0300

    Update main.cpp

--- lab5/ex2/main.cpp
+++ lab5/ex2/main.cpp
@@ -1,19 +1,19 @@
 #include "ex2.hpp"
 
 int main(){
-  std::vector<uint8_t> byte_vector;
+	std::vector<uint8_t> byte_vector;
 
-  // Добавляем байты в вектор
-  byte_vector.push_back(123);
-  byte_vector.push_back(12);
-  byte_vector.push_back(32);
+	// Добавляем байты в вектор
+	byte_vector.push_back(123);
+	byte_vector.push_back(12);
+	byte_vector.push_back(32);
 
-  encoder enc(byte_vector);
-	try{
+	encoder enc(byte_vector);
+	try {
 		enc.encode("input_file", "output", true);
 		enc.encode("output", "rr.txt", false);
-        } catch (const std::invalid_argument &) {
-          std::cout << "File didn't open\n";
+	} catch (const std::invalid_argument &) {
+		std::cout << "File didn't open\n";
 		return 1;
 	}
 }
